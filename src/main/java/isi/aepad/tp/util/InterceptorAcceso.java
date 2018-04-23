e477114bd04c6e94c9d995a65d6287137f061b58
mart-dominguez <mdomingu@gmail.com>
2018-04-23 13:47:50 -0300

    corregir @interceptor

--- src/main/java/isi/aepad/tp/util/InterceptorAcceso.java
+++ src/main/java/isi/aepad/tp/util/InterceptorAcceso.java
@@ -8,11 +8,13 @@ import java.util.logging.Logger;
 
 import javax.annotation.Resource;
 import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.sql.DataSource;
 
 import isi.aepad.tp.modelo.LogAcceso;
 
+@Interceptor
 public class InterceptorAcceso {
 
 	@Resource(lookup = "jdbc/aepad")
